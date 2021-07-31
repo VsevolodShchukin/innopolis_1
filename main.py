5cc6a58a8149d9d8c662d5acba19f7c6e6b4b237
vsevo <vsevolod.schukin@gmail.com>
2021-07-31 20:00:19 +0300

    Удаление старых файлов и добавление обновленного main.py

--- main.py
+++ main.py
@@ -1,136 +1,90 @@
+"""Импорт модуля random и sys."""
 import random
+import sys
+
+monster_counter = 0
+hp = random.randint(10, 20)
+attack = random.randint(10, 15)
 
 
-def game():
+def start_game() -> None:
+    """Функция, отображающая стартовые показатели игрока."""
+    print(f"Твое здоровье - {hp}, твоя атака - {attack}")
 
-    # Игровые показатели
-    monster_counter = 10
-    hp = random.randint(10, 20)
-    attack = random.randint(10, 15)
-    print(
-        f"Ты рыцарь, который должен защитить королевство! Твое количество жизней - {hp}. Твоя сила удара - {attack}"
-    )
 
-    # Функции оповещения игрока о событии
-    class Alarms:
-        # Сообщает о встреченном чудовище. Отражает его характеристики.
-        def alarm_mosnter(self, monster_hp, monster_attack):
-            print(
-                f"БОЙ! Ты встретил чудовище c {monster_hp} жизнями и с силой удара {monster_attack}. Будешь с ним сражаться?"
-            )
+def get_apple() -> None:
+    """Функция, генеририрующая яблоко. Оно добавляет ед. жизни к показателю hp."""
+    global hp
+    apple = random.randint(1, 3)
+    hp = hp + apple
+    print(f"Ты нашел яблоко! Оно увеличило количество жизней на {apple}. Теперь твое количество жизней: {hp}")
 
-        # Сообщает о найденном мече. Отражает его характеристики.
-        def alarm_swrd(self, power_swrd):
-            print(
-                f"Ты нашел МЕЧ! Его сила атаки - {power_swrd}. Хочешь его взять на замену старому?"
-            )
 
-        # Сообщает о найденном яблоке. Показывает, сколько оно прибавило жизней и чему равняется значение жизней теперь.
-        def alarm_apple(self, apple):
-            print(
-                f"Ты нашел яблоко и сразу же съел его! Оно увеличило количество жизней на {apple}. Теперь твое количество жизней: {hp + apple}"
-            )
+def get_sword() -> None:
+    """Функция, генеририрующая меч. Меч изменяет показатели attack игрока."""
+    global attack
+    sword = random.randint(10, 25)
+    print(f"Ты нашел MEЧ! Его сила атаки - {sword}.")
+    decision = input("Введи 1 - чтобы взять, 2 - чтобы пройти мимо: ")
+    while decision not in ("1", "2"):
+        print("Некорректный ввод")
+        decision = input("Введи 1 - чтобы взять, 2 - чтобы пройти мимо: ")
+    if decision == "1":
+        attack = sword
+        print(f"Ты взял себе этот меч. Теперь твоя сила твоей атаки: {attack}")
+    elif decision == "2":
+        pass
 
-    # Генераторы случайных сущностей и функции с логикой игры
-    class Game_logic(Alarms):
 
-        # Генератор случайных монстров с вызовом функций оповещения и выбора
-        def generator_monster(self):
-            global monster_hp
-            global monster_attack
-            monster_hp = random.randint(5, 10)
-            monster_attack = random.randint(3, 10)
-            self.alarm_mosnter(monster_hp, monster_attack)
-            self.choose_your_destiny()
-
-        # Генератор случайных мечей с вызовом функции оповещения и функции выбора внутри
-        def generator_swrd(self):
-            power_swrd = random.randint(10, 25)
-            self.alarm_swrd(power_swrd)
-
-            def decision_swrd():
-                decision = input("Введи 1 - чтобы взять, 2 - чтобы пройти мимо: ")
-                global attack
-                if decision == "1":
-                    attack = power_swrd
-                    print(
-                        f"Ты взял себе этот меч. Теперь твоя сила твоей атаки: {attack}"
-                    )
-                elif decision == "2":
-                    pass
-                else:
-                    print("Некорректный ввод!")
-                    decision_swrd()
-
-            decision_swrd()
-
-        # Генератор случайных яблок с вызовом функции оповещения. Изменяет количество жизней
-        def generator_apple(self):
-            apple = random.randint(1, 3)
-            self.alarm_apple(apple)
-            global hp
-            hp = hp + apple
-
-        # Функция, которая обрабатывает выбор игрока при событии "Бой"
-        def choose_your_destiny(self):
-            decision = input(
-                "Введи 1 - чтобы принять бой, 2 - чтобы убежать и набраться сил: "
-            )
-            if decision == "1":
-                self.battle()
-            elif decision == "2":
-                print("Ты убежал от чудовища!")
-                pass
-            else:
-                print("Некорректный ввод. Введи 1 или 2")
-                self.choose_your_destiny()
-
-        # Функция с логикой боя
-        def battle(self):
-            global monster_hp
-            global monster_attack
-            global attack
-            global hp
-            global monster_counter
+def fight_with_monster() -> None:
+    """Функция, генеририрующая чудовища. Обрабатывает логику боя."""
+    global hp
+    global attack
+    global monster_counter
+    monster_hp = random.randint(5, 10)
+    monster_attack = random.randint(3, 10)
+    print(f"БОЙ! Ты встретил чудовище c {monster_hp} жизнями и с силой удара {monster_attack}.")
+    decision = input("Введи 1 - чтобы принять бой, 2 - чтобы убежать и набраться сил: ")
+    while decision not in ("1", "2"):
+        print("Некорректный ввод")
+        decision = input("Введи 1 - чтобы принять бой, 2 - чтобы убежать и набраться сил: ")
+    if decision == "1":
+        while hp > 0 or monster_hp > 0:
+            hp = hp - monster_attack
+            monster_hp = monster_hp - attack
             if hp <= 0:
-                print("ПОРАЖЕНИЕ! Игра окончена!")
-            elif monster_counter == 1 and monster_hp <= 0:
-                monster_counter = 0
-                print("ПОБЕДА! Ты победил всех чудовищ")
-            elif monster_hp <= 0:
-                monster_counter = monster_counter - 1
-                print(
-                    f"Ты победил чудовище! Тебе осталось уничтожить еще {monster_counter}..."
-                )
+                continue
+            if monster_hp <= 0:
+                if monster_counter == 9:
+                    monster_counter = monster_counter + 1
+                    break
+                monster_counter = monster_counter + 1
+                print("Ты победил!")
+                print(f"Тебе осталось уничтожить еще {10 - monster_counter}...")
                 print(f"Твое количество жизней - {hp},твоя атака - {attack}.")
-            else:
-                monster_hp = monster_hp - attack
-                hp = hp - monster_attack
-                self.battle()
+            break
+    elif decision == "2":
+        pass
 
-    # Генератор случайных событий и инспектор условий
-    class Creator(Game_logic):
 
-        # Инспектор условий игры
-        def inspector_world(self):
-            global hp
-            global monster_counter
-            while hp > 0 and monster_counter > 0:
-                self.random_generator()
-
-        # Генератор случайных событий
-        def random_generator(self):
-            i = random.randint(1, 3)
-            if i == 1:
-                self.generator_monster()
-            elif i == 2:
-                self.generator_apple()
-            else:
-                self.generator_swrd()
-                self.inspector_world()
-
-    a = Creator()
-    a.inspector_world()
+def game() -> None:
+    """Функция, генерируюшая случайность событий. Запускает и завершает игру."""
+    global hp
+    global monster_counter
+    while hp > 0 and monster_counter != 10:
+        i = random.randint(1, 11)
+        if i in range(1, 6):
+            fight_with_monster()
+        elif i in range(6, 8):
+            get_apple()
+        elif i in range(8, 11):
+            get_sword()
+    if hp <= 0:
+        print("ПОРАЖЕНИЕ! Игра окончена!")
+        sys.exit()
+    if monster_counter == 10:
+        print("ПОБЕДА! Ты победил всех чудовищ")
+        sys.exit()
 
 
 game()
